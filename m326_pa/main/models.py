0199cd3b33564a6b5e5e9b51c23ad672c3464c25
Architeutes <architeutes440@gmail.com>
2022-09-28 09:28:00 +0200

    Added first version of competence models
    -Added the 3 models Top-, Mid- and LowLevelCompetence for the required 3 levels.
    -The levels are done as seperate models for the case that they need different properties but mostly to allow for easier and cleaner management of deletion as this way the foreign keys can be set to onDelete cascade which deletes all the lower level models from the db aswell when a top level is deleted.
    -all of this could also be done with just a property defining the level of a competence and all just in one model but it would be way more complicated and in django its more optimal to work like this and use the systems already in place instead of building something weird yourself
    -the models now all have the properties: name, description and for the two lover levels parentCompetence as foreign key to the level above them.
    -all competences have the __str__ method manually defined so when you print them for some reason we can now controll what exactly is printed instead of printing just the whole object. in our case the name is being returned.
    -also the plural name is set for all of them which is just a style option as django per default just adds an s and defining it manually is safer and cleaner. this will matter in the admin panel where they will be named in plural. (the models in the code are not named plural (example:LowLevelCompetence))

--- m326_pa/main/models.py
+++ m326_pa/main/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class TopLevelCompetence(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Top Level Competences"
+
+
+
+
+
+class MidLevelCompetence(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    parentCompetence = models.ForeignKey(TopLevelCompetence, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Mid Level Competences"
+
+
+
+
+
+class LowLevelCompetence(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    midlevelkompetenz = models.ForeignKey(MidLevelCompetence, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Low Level Competences"
