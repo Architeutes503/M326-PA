a7e0c27b8609c3369f1cf5660d25fbc72d8b610d
Architeutes <architeutes440@gmail.com>
2022-10-31 12:16:48 +0100

    started working on expanding models and added drawio
    - started drawio to  plan db
    - added Job to models so teachers and competences can have a specific job assigned
    - added teachers that have a name description and job
    -started working on how to organize a teachers competences, planned and achieved competences but this is not yet finished and needs more work and planning
    - registered new models to admin panel

--- m326_pa/main/admin.py
+++ m326_pa/main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import CompetenceCategory, CompetenceLevel, Competence, Ressource
+from .models import CompetenceCategory, CompetenceLevel, Competence, Ressource, Job, CompetenceProfile, AchievedCompetence, PlannedCompetences, Teacher
 
 #also show id in admin
 class CompetenceCategoryAdmin(admin.ModelAdmin):
@@ -20,9 +20,38 @@ class CompetenceAdmin(admin.ModelAdmin):
     search_fields = ('name', 'description')
     list_per_page = 25
 
-
 class RessourceAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'description', 'url', 'updatedAt', 'createdAt')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', 'description')
+    list_per_page = 25
+
+class JobAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description', 'updatedAt', 'createdAt')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', 'description')
+    list_per_page = 25
+
+class CompetenceProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description', 'updatedAt', 'createdAt')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', 'description')
+    list_per_page = 25
+
+class AchievedCompetenceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'competence', 'achievedAt', 'updatedAt', 'createdAt')
+    list_display_links = ('id', 'competence')
+    search_fields = ('competence__name', 'competence__description')
+    list_per_page = 25
+
+class PlannedCompetencesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'competence', 'plannedAt', 'updatedAt', 'createdAt')
+    list_display_links = ('id', 'competence')
+    search_fields = ('competence__name', 'competence__description')
+    list_per_page = 25
+
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description','updatedAt', 'createdAt')
     list_display_links = ('id', 'name')
     search_fields = ('name', 'description')
     list_per_page = 25
@@ -32,3 +61,8 @@ admin.site.register(CompetenceCategory, CompetenceCategoryAdmin)
 admin.site.register(CompetenceLevel, CompetenceLevelAdmin)
 admin.site.register(Competence, CompetenceAdmin)
 admin.site.register(Ressource, RessourceAdmin)
+admin.site.register(Job, JobAdmin)
+admin.site.register(CompetenceProfile, CompetenceProfileAdmin)
+admin.site.register(AchievedCompetence, AchievedCompetenceAdmin)
+admin.site.register(PlannedCompetences, PlannedCompetencesAdmin)
+admin.site.register(Teacher, TeacherAdmin)
--- m326_pa/main/models.py
+++ m326_pa/main/models.py
@@ -1,3 +1,5 @@
+from tabnanny import check
+from xml.etree.ElementInclude import LimitedRecursiveIncludeError
 from django.db import models
 from django.utils import timezone
 
@@ -45,6 +47,24 @@ class CompetenceLevel(models.Model):
 
 
 
+class Job(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    updatedAt = models.DateTimeField(default=timezone.now)
+    createdAt = models.DateTimeField(editable=False)
+
+    def save(self, *args, **kwargs):
+        ''' On save, update timestamps '''
+        if not self.id:
+            self.createdAt = timezone.now()
+        self.updatedAt = timezone.now()
+        return super(Job, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+
 class Ressource(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True)
@@ -67,13 +87,13 @@ class Ressource(models.Model):
 
 
 
-
 class Competence(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     ressources = models.ManyToManyField(Ressource, blank=True)
     competenceCategory = models.ForeignKey(CompetenceCategory, on_delete=models.CASCADE)
     competenceLevel = models.ForeignKey(CompetenceLevel, on_delete=models.SET_DEFAULT, default=1)
+    job = models.ForeignKey(Job, on_delete=models.SET_NULL, null=True, blank=True)
     updatedAt = models.DateTimeField(default=timezone.now)
     createdAt = models.DateTimeField(editable=False)
 
@@ -87,5 +107,116 @@ class Competence(models.Model):
     class Meta:
         verbose_name_plural = "Competences"
     
+    def __str__(self):
+        return self.name
+
+
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    job = models.ForeignKey(Job, on_delete=models.SET_NULL, null=True, blank=True)
+    updatedAt = models.DateTimeField(default=timezone.now)
+    createdAt = models.DateTimeField(editable=False)
+
+    def save(self, *args, **kwargs):
+        ''' On save, update timestamps '''
+        if not self.id:
+            self.createdAt = timezone.now()
+        self.updatedAt = timezone.now()
+        return super(Teacher, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name_plural = "Teachers"
+    
+    def __str__(self):
+        return self.name
+
+
+
+class AvailableCompetence(models.Model):
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    competence = models.ForeignKey(Competence, on_delete=models.CASCADE)
+    updatedAt = models.DateTimeField(default=timezone.now)
+    createdAt = models.DateTimeField(editable=False)
+
+    def save(self, *args, **kwargs):
+        ''' On save, update timestamps '''
+        if not self.id:
+            self.createdAt = timezone.now()
+        self.updatedAt = timezone.now()
+        return super(AvailableCompetence, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name_plural = "Avilable Competences"
+
+    def __str__(self):
+        return f"{self.competence.name} - {self.teacher.name}"
+
+
+class AchievedCompetence(models.Model):
+    competence = models.ForeignKey(Competence, on_delete=models.CASCADE)
+    achievedAt = models.DateTimeField(default=timezone.now)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, null=True, blank=True)
+    updatedAt = models.DateTimeField(default=timezone.now)
+    createdAt = models.DateTimeField(editable=False)
+
+    def save(self, *args, **kwargs):
+        ''' On save, update timestamps '''
+        if not self.id:
+            self.createdAt = timezone.now()
+        self.updatedAt = timezone.now()
+        return super(AchievedCompetence, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name_plural = "Achieved Competences"
+
+    def __str__(self):
+        return self.competence.name
+
+
+
+class PlannedCompetences(models.Model):
+    competence = models.ForeignKey(Competence, on_delete=models.CASCADE)
+    plannedAt = models.DateTimeField(default=timezone.now)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, null=True, blank=True)
+    updatedAt = models.DateTimeField(default=timezone.now)
+    createdAt = models.DateTimeField(editable=False)
+
+    def save(self, *args, **kwargs):
+        ''' On save, update timestamps '''
+        if not self.id:
+            self.createdAt = timezone.now()
+        self.updatedAt = timezone.now()
+        return super(PlannedCompetences, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name_plural = "Planned Competences"
+
+    def __str__(self):
+        return self.competence.name
+
+
+
+class CompetenceProfile(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    teacher = models.ForeignKey(Teacher, on_delete=models.SET_NULL, null=True, blank=True)
+    competences = models.ManyToManyField(AvailableCompetence, blank=True)
+    achievedCompetences = models.ManyToManyField(AchievedCompetence, blank=True)
+    plannedCompetences = models.ManyToManyField(PlannedCompetences, blank=True)
+    updatedAt = models.DateTimeField(default=timezone.now)
+    createdAt = models.DateTimeField(editable=False)
+
+    def save(self, *args, **kwargs,):
+        ''' On save, update timestamps '''
+        if not self.id:
+            self.createdAt = timezone.now()
+        self.updatedAt = timezone.now()
+        return super(CompetenceProfile, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name_plural = "Competence Profiles"
+
     def __str__(self):
         return self.name
